574c9851e73ac97d48c1138dd10fa93af2771087
mattscilipoti <matt.scilipoti@ga.co>
2016-05-16 13:24:23 -0400

    Add Course (just model/migration)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Course.destroy_all
+
+Course.create!(name: 'TEST COURSE1')
